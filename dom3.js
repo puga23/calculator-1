1cf1fd7c9c337b25f32e5c41c34a0f44d677df10
karlo699 <khosiashvilik@gmail.com>
2024-04-12 22:16:59 +0400

    gtyu

---
+++ dom3.js
@@ -0,0 +1,28 @@
+const display = document.querySelector(".display");
+
+console.dir(display);
+
+function btnText(x) {
+  if (
+    display.textContent === "" &&
+    (x === "-" || x === "+" || x === "/" || x === "*")
+  ) {
+    display.textContent = "";
+  } else if (display.textContent === "" && x === ".") {
+    display.textContent = "0.";
+  } else {
+    display.textContent = display.textContent + x;
+  }
+}
+
+function rstText() {
+  display.textContent = "";
+}
+
+function toloba() {
+  display.textContent = eval(display.textContent);
+}
+
+function dltText() {
+  display.textContent = display.textContent.slice(0, -1);
+}
