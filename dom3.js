f5b59894d6f63a1eb40ebe0c558a930df3db00a2
Ilia Tskhvediani <107941505+Ilia898@users.noreply.github.com>
2024-04-13 15:33:21 +0400

    1

--- dom3.js
+++ dom3.js
@@ -3,16 +3,19 @@ const display = document.querySelector(".display");
 console.dir(display);
 
 function btnText(x) {
-  if (
-    display.textContent === "" &&
-    (x === "-" || x === "+" || x === "/" || x === "*")
-  ) {
-    display.textContent = "";
-  } else if (display.textContent === "" && x === ".") {
-    display.textContent = "0.";
-  } else {
-    display.textContent = display.textContent + x;
+  if(display.textContent.length <= 18){
+    if (
+      display.textContent === "" &&
+      (x === "-" || x === "+" || x === "/" || x === "*")
+    ) {
+      display.textContent = "";
+    } else if (display.textContent === "" && x === ".") {
+      display.textContent = "0.";
+    } else {
+      display.textContent = display.textContent + x;
+    }
   }
+
 }
 
 function rstText() {
