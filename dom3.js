6ea5b54dfdc7be46cbe057b0d9fa326f315765f0
tekhova29 <160626673+tekhova29@users.noreply.github.com>
2024-04-13 22:17:28 +0400

    cal

--- dom3.js
+++ dom3.js
@@ -1,9 +1,44 @@
 const display = document.querySelector(".display");
+const btn1 = document.querySelector(".theme-btn1")
+const btn2 = document.querySelector(".theme-btn2")
+const btn3 = document.querySelector(".theme-btn3")
+const div3 = document.querySelector(".div3")
+
 
 console.dir(display);
 
+btn1.onclick=function(){
+  btn1.style.opacity= "1"
+  btn2.style.opacity= "0"
+  btn3.style.opacity= "0"
+}
+
+btn2.onclick=function(){
+  btn1.style.opacity= "0"
+  btn2.style.opacity= "1"
+  btn3.style.opacity= "0" 
+  document.body.style.backgroundColor= "#E6E6E6"
+  document.querySelector(".div123").style.color = "#36362C"
+}
+
+btn3.onclick=function(){
+  btn1.style.opacity= "0"
+  btn2.style.opacity= "0"
+  btn3.style.opacity= "1"
+   document.body.style.backgroundColor= "#E6E6E6"
+}
+
+
+// div3.addEventListener("click",(event)=> {
+//   console.log (event);
+//   event.target.style.opacity="1"
+// })
+
+
+
 function btnText(x) {
- 
+  console.log(display.textContent.length);
+  if (display.textContent.length < 10) {
     if (
       display.textContent === "" &&
       (x === "-" || x === "+" || x === "/" || x === "*")
@@ -11,10 +46,12 @@ function btnText(x) {
       display.textContent = "";
     } else if (display.textContent === "" && x === ".") {
       display.textContent = "0.";
+    } else if (display.textContent === "0" && x !== ".") {
+      display.textContent = x;
     } else {
       display.textContent = display.textContent + x;
     }
-
+  }
 }
 
 function rstText() {
